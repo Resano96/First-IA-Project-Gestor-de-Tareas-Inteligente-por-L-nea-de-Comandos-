a96f0a65fdcf513fe6d4311223ad1d4d6979314b
Ander Resano Farelo <ander.resano@gmail.com>
2025-10-08 12:29:17 +0200

    Implementado gestor de tareas simple

---
+++ main.py
@@ -0,0 +1,50 @@
+from taskmanager import TaskManager
+
+
+
+def print_menu():
+    print("\n---------Gestor de tareas Inteligente------")
+    print("1. Añadir tarea")
+    print("2. Listar tareas")
+    print("3. Completar tarea")
+    print("4. Eliminar tarea")
+    print("5. Salir")
+
+
+
+
+def main():
+
+    manager = TaskManager()
+
+    while True:
+
+        print_menu()
+    
+        choice = input("Elige una opcion: ")
+
+        match choice:
+
+            case "1":
+                description = input("Descripcion de la tarea: ")
+                manager.add_task(description)
+
+            case "2":
+                manager.list_task()
+
+            case "3":
+                id= input("Id de tarea a completar")
+                manager.complete_task(id)
+            case "4":
+                id= input("Id de tarea a eliminar")
+                manager.delete_task(id)
+            case "5":
+                print("Saliendo...")
+                break
+            case _:
+                print("Opcion no valida. Selecciona otra.")
+    
+
+
+if __name__ == "__main__":
+    main()
