b2c188f03f9491a5a7c6698331f3ed6f9e12b7a1
Ander Resano Farelo <ander.resano@gmail.com>
2025-10-08 13:21:56 +0200

    Implementada persistencia

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from taskmanager import TaskManager
 
-
-
 def print_menu():
     print("\n---------Gestor de tareas Inteligente------")
     print("1. Añadir tarea")
@@ -9,8 +7,6 @@ def print_menu():
     print("3. Completar tarea")
     print("4. Eliminar tarea")
     print("5. Salir")
-
-
 
 
 def main():
@@ -21,29 +17,35 @@ def main():
 
         print_menu()
     
-        choice = input("Elige una opcion: ")
+        try: 
 
-        match choice:
+            choice = int(input("Elige una opcion: "))
 
-            case "1":
-                description = input("Descripcion de la tarea: ")
-                manager.add_task(description)
+            match choice:
 
-            case "2":
-                manager.list_task()
+                case 1:
+                    description = input("Descripcion de la tarea: ")
+                    manager.add_task(description)
 
-            case "3":
-                id= input("Id de tarea a completar")
-                manager.complete_task(id)
-            case "4":
-                id= input("Id de tarea a eliminar")
-                manager.delete_task(id)
-            case "5":
-                print("Saliendo...")
-                break
-            case _:
-                print("Opcion no valida. Selecciona otra.")
-    
+                case 2:
+                    manager.list_task()
+
+                case 3:
+                    id= int(input("Id de tarea a completar"))
+                    manager.complete_task(id)
+
+                case 4:
+                    id= int(input("Id de tarea a eliminar"))
+                    manager.delete_task(id)
+
+                case 5:
+                    print("Saliendo...")
+                    break
+
+                case _:
+                    print("Opcion no valida. Selecciona otra.")
+        except ValueError:
+            print("Opcion no valida. Selecciona otra.")
 
 
 if __name__ == "__main__":
--- taskmanager.py
+++ taskmanager.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Task:
 
     def __init__(self, id, description, completed=False):
@@ -6,19 +9,24 @@ class Task:
         self.completed  = completed
 
     def __str__(self):
-        status = "👍" if self.completed else "👎"
+        status = "✔️" if self.completed else "❌"
         return f"[{status}] #{self.id}: {self.description}"
     
 class TaskManager:
+
+    FILENAME = "task.json"
+
     def __init__(self):
         self._tasks=[]
         self._next_id=1
+        self.load_tasks()
 
     def add_task(self, description):
         task = Task(self._next_id, description)
         self._tasks.append(task)
         self._next_id +=1
         print(f"Tarea añadida: {description}")
+        self.save_tasks()
 
     def list_task(self):
         if not self._tasks:
@@ -32,6 +40,7 @@ class TaskManager:
             if task.id == id:
                 task.completed = True
                 print(f"Tarea completada: {task}")
+                self.save_tasks()
                 return
         print(f"Tarea no encontrada: #{id}")
     
@@ -40,5 +49,22 @@ class TaskManager:
             if task.id == id:
                 self._tasks.remove(task)
                 print(f"Tarea eliminada: {id}")
+                self.save_tasks()
                 return
         print(f"Tarea no encontrada: #{id}")
+
+    def load_tasks(self):
+        try:
+            with open(self.FILENAME, "r") as file:
+                data = json.load(file)
+                self._tasks= [Task(item["id"], item["description"], item["completed"])for item in data]
+                if self._tasks:
+                    self._next_id = self._tasks[-1].id + 1
+                else:
+                    self._next_id = 1
+        except FileNotFoundError:
+            self._tasks = []
+
+    def save_tasks(self):
+        with open(self.FILENAME, "w") as file:
+            json.dump([{"id": task.id, "description": task.description, "completed": task.completed} for task in self._tasks], file, indent=4)
